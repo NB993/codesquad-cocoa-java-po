613902b3638d6d0945fd99623ec17c91d9f8ce82
NB993 <qnalsfj@gmail.com>
2021-11-03 21:08:39 +0900

    ArrayList로 변경

--- src/day3/mission0/PrintRandomPeople.java
+++ src/day3/mission0/PrintRandomPeople.java
@@ -6,7 +6,7 @@ public class PrintRandomPeople {
 
   public static void main(String[] args) {
     int headCount = Prompt.inputHeadCount();
-    List<String> memberList = MemberList.of(headCount);
+    ArrayList<String> memberList = MemberList.of(headCount);
 
     Prompt.print(memberList);
   }
