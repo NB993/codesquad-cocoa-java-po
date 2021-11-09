924df5698a40ad145e85eb37f88a33607ccf258e
NB993 <qnalsfj@gmail.com>
2021-11-10 02:40:37 +0900

    Print클래스 printHangman메서드 구현
    Game클래스 start메서드에서 문제를 풀다 오답을 제출한 경우
    HangMan객체의 currentHangManStack변수에 출력할 문자열을 한줄씩 쌓고,
    HangMan을 출력할 때 printHangMan메서드에 HangMan인스턴스를 넘겨준다.
    HangMan에 toString을 오버라이딩해서 currentHangManStack변수만
    출력해주도록 했다.

--- src/day6/hangman/Print.java
+++ src/day6/hangman/Print.java
@@ -2,4 +2,17 @@ package day6.hangman;
 
 public class Print {
 
+  String answer;
+
+  public void printUserLife(int life) {
+    System.out.println("남은 기회는 : " + life + "개입니다.");
+  }
+
+  public void printHangman(HangMan hangMan) {
+    System.out.println(hangMan);
+  }
+
+  public void printBlankOfAnswerLength(String answer) {
+  }
+
 }
