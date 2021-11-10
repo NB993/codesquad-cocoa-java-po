c8b63ab8c546c02aad40782fcfe327e608f33c86
NB993 <qnalsfj@gmail.com>
2021-11-10 11:07:54 +0900

    addSpaceBetweenEach메서드 구현
    answer blank 사이사이 공백을 두어 정답이 몇 글자인지 알아보기
    쉽게 해줌

--- src/day6/hangman/Print.java
+++ src/day6/hangman/Print.java
@@ -3,6 +3,7 @@ package day6.hangman;
 public class Print {
 
   String answer;
+  String answerBlank;
 
   public void printUserLife(int life) {
     System.out.println("남은 기회는 : " + life + "개입니다.");
@@ -12,7 +13,31 @@ public class Print {
     System.out.println(hangMan);
   }
 
-  public void printBlankOfAnswerLength(String answer) {
+  public void printAnswerBlank(String answer) {
+
+    addSpaceBetweenEach();
+  }
+
+  public void retainAnswer(String answer) {
+    this.answer = answer;
+  }
+
+  public void makeBlankVisibleAlphabet(String alphabet) {
+    int index;
+
+    while ((index = answer.indexOf(alphabet)) > -1) {
+
+    }
+  }
+
+  private void addSpaceBetweenEach() {
+    String[] splitedAnswerBlank = answerBlank.split("");
+
+    System.out.println(String.join(" ", splitedAnswerBlank);
+  }
+
+  public void printGotRight(String alphabet) {
+    makeBlankVisibleAlphabet(alphabet);
   }
 
 }
