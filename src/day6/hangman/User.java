16f9da762fc9d0beb3bc32e57f54121935ef599c
NB993 <qnalsfj@gmail.com>
2021-11-10 14:07:13 +0900

    answerCount멤버 추가
    한 단어를 맞췄을 때 User클래스 answerCountUp메서드를 호출해서
    맞춘 정답 개수를 증가시킵니다. 추후 랭킹기능을 추가할시 사용.

--- src/day6/hangman/User.java
+++ src/day6/hangman/User.java
@@ -3,6 +3,7 @@ package day6.hangman;
 public class User {
   private String name;
   private int life = 10;
+  private int answerCount = 0;
 
   public User(String name) {
     this.name = name;
@@ -12,4 +13,15 @@ public class User {
     life--;
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public int getLife() {
+    return life;
+  }
+
+  public void answerCountUp() {
+    answerCount++;
+  }
 }
