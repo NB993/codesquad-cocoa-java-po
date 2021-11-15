1ef92ed03c41b53d220c8cbbedddd2fb9712e42f
jaehongChoe <qnalsfj@gmail.com>
2021-11-16 00:35:10 +0900

    CommandList클래스 input메서드, getter메서드 수정
    input메서드는 input멤버변수에 값을 할당만 하도록 변경
    getter메서드 이름 변경.

--- src/day11/CommandLine.java
+++ src/day11/CommandLine.java
@@ -1,2 +1,24 @@
-package day11;public class CommandLine {
+package day11;
+
+import java.io.IOException;
+import java.util.*;
+
+public class CommandLine {
+
+  Scanner sc = new Scanner(System.in);
+  String[] input;
+
+  public void input() throws IllegalArgumentException {
+    System.out.print("posh>");
+    input = sc.nextLine().split(" ");
+  }
+
+  public String getCommand() {
+    return input[0];
+  }
+
+  public String getText() {
+    return input[1];
+  }
+
 }
