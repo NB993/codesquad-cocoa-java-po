289beec0f5997b10f631f26caa23d2e65babb340
jaehongChoe <qnalsfj@gmail.com>
2021-11-23 21:48:42 +0900

    ResultPanel 사원 검색 결과 패널
    검색 결과가 출력되는 패널이다.

--- src/week4/day17/ResultPanel.java
+++ src/week4/day17/ResultPanel.java
@@ -1,2 +1,40 @@
-package week4.day17;public class ResultPanel {
+package week4.day17;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextField;
+
+public class ResultPanel extends Panel {
+
+  private final Panel panel;
+
+  public ResultPanel() {
+    this.panel = new Panel();
+    initPanel();
+  }
+
+  private void initPanel() {
+    panel.setBackground(Color.LIGHT_GRAY);
+    panel.setSize(300, 200);
+    panel.setLocation(400, 40);
+
+    Label lbName = new Label("사번: ");
+    TextField textName = new TextField(20);
+    textName.setSize(100, 20);
+
+    Button btnSearch = new Button("검색");
+    btnSearch.addActionListener((e) -> {
+      System.out.println(textName.getText());
+    });
+
+    panel.add(lbName);
+    panel.add(textName);
+    panel.add(btnSearch);
+  }
+
+  public Panel getPanel() {
+    return panel;
+  }
 }
