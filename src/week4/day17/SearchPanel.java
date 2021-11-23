8ed7203ce1978ccaa41fb6b96b07381213951df9
jaehongChoe <qnalsfj@gmail.com>
2021-11-23 21:47:28 +0900

    SearchPanel 사원을 검색하는 패널
    부서코드, 이름, 사번으로 검색할 수 있는 패널이다.
    검색 버튼 클릭시 일치하는 데이터를 EmployeeStore에서 검색하여
    ResultPanel로 출력해준다.

--- src/week4/day17/SearchPanel.java
+++ src/week4/day17/SearchPanel.java
@@ -1,2 +1,41 @@
-package week4.day17;public class SearchPanel {
+package week4.day17;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextField;
+
+public class SearchPanel extends Panel {
+
+  private final Panel panel;
+
+  public SearchPanel() {
+    this.panel = new Panel();
+    initPanel();
+  }
+
+  private void initPanel() {
+    panel.setBackground(Color.LIGHT_GRAY);
+    panel.setSize(300, 200);
+    panel.setLocation(50, 40);
+
+    Label lbName = new Label("사번: ");
+    TextField textName = new TextField(20);
+    textName.setSize(100, 20);
+
+    Button btnSearch = new Button("검색");
+    btnSearch.addActionListener((e) -> {
+      System.out.println(textName.getText());
+    });
+
+    panel.add(lbName);
+    panel.add(textName);
+    panel.add(btnSearch);
+  }
+
+  public Panel getPanel() {
+    return panel;
+  }
 }
