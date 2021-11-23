08d4c228a1ea400fdd17d10cbc1eae686669d702
jaehongChoe <qnalsfj@gmail.com>
2021-11-23 21:45:50 +0900

    EmployeeSearchFrame 메인 프레임 클래스
    디폴트 width, heigth를 가지고 있으며, 생성과 동시에
     initFrame메서드로 위치와 크기를 초기화한다.

--- src/week4/day17/EmployeeSearchFrame.java
+++ src/week4/day17/EmployeeSearchFrame.java
@@ -1,2 +1,41 @@
-package week4.day17;public class EmployeeSearchFrame {
+package week4.day17;
+
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+
+public class EmployeeSearchFrame {
+  static final int DEFAULT_FRAME_WIDTH = 960;
+  static final int DEFAULT_FRAME_HEIGHT = 540;
+
+  private final Frame frame;
+
+  public EmployeeSearchFrame() {
+    this(DEFAULT_FRAME_WIDTH, DEFAULT_FRAME_HEIGHT);
+  }
+
+  public EmployeeSearchFrame(int width, int height) {
+    this.frame = new Frame();
+    initFrame(width, height);
+  }
+
+  private void initFrame(int width, int height) {
+    frame.setLayout(null);
+    frame.setSize(width, height);
+
+    Toolkit tk = Toolkit.getDefaultToolkit();
+    Dimension screenSize = tk.getScreenSize();
+    frame.setLocation((screenSize.width / 2) - (width / 2), (screenSize.height / 2) - (height / 2));
+    frame.addWindowListener(new AdapterWindowListener() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        e.getWindow().dispose();
+      }
+    });
+  }
+
+  public Frame getFrame() {
+    return frame;
+  }
 }
