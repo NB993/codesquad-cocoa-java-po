47c12fa3d778e5800795a7db096a7fb0a3ed280e
jaehongChoe <qnalsfj@gmail.com>
2021-11-23 22:54:14 +0900

    EmployeeStore makeEmployee메서드 추가
    파일로 읽어온 사원 정보 한 줄을 가지고 Employee객체를 생성

--- src/week4/day17/employee_search/EmployeeStore.java
+++ src/week4/day17/employee_search/EmployeeStore.java
@@ -1,18 +1,42 @@
 package week4.day17.employee_search;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import week2.day6.hangman.Problem;
 
 public class EmployeeStore {
+
   private final List<Employee> employees;
 
   public EmployeeStore() {
     this.employees = new ArrayList<>();
-    loadEmployees();
+    loadEmployeesFile();
   }
 
   //TODO: 파일로 직원목록 불러오기
-  private void loadEmployees() {
+  private void loadEmployeesFile() {
 
+    try {
+      String path = this.getClass().getResource("").getPath();
+      System.out.println(path);
+      FileReader fr = new FileReader(path + "/employees.txt", Charset.forName("UTF-8"));
+      BufferedReader br = new BufferedReader(fr);
+      String singleLine = "";
+      String[] employeeData;
+
+      while ((singleLine = br.readLine()) != null) {
+        employees.add(makeEmployee(singleLine.split(" ")));
+      }
+    } catch (IOException e) {
+      System.out.println("문제 파일을 읽어오는 도중 에러가 발생하였습니다.");
+    }
+  }
+
+  private Employee makeEmployee(String[] employeeDate) {
+    return new Employee(employeeDate[0], employeeDate[1], employeeDate[2]);
   }
 }
