5e6df45743bf4f63268c4dd2ab6d6f89ed175e9c
jaehongChoe <qnalsfj@gmail.com>
2021-11-23 21:50:04 +0900

    윈도우 창을 닫는 메서드만 구현한 어댑터

--- src/week4/day17/AdapterWindowListener.java
+++ src/week4/day17/AdapterWindowListener.java
@@ -1,2 +1,42 @@
-package week4.day17;public class AdapterWindowListener {
+package week4.day17;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class AdapterWindowListener implements WindowListener {
+
+  @Override
+  public void windowOpened(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowClosing(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowClosed(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowIconified(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowDeiconified(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowActivated(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowDeactivated(WindowEvent e) {
+
+  }
 }
