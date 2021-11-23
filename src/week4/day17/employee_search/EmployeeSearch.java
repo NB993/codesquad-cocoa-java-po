c9ef86074ae4abdf0538b264a684689be8c06811
jaehongChoe <qnalsfj@gmail.com>
2021-11-24 02:02:48 +0900

    EmployeeSearch 메인 메서드 흐름
    사원정보 파일 로딩 후
    프레임 생성하고, 패널 두개 생성하고 출력

--- src/week4/day17/employee_search/EmployeeSearch.java
+++ src/week4/day17/employee_search/EmployeeSearch.java
@@ -1,13 +1,18 @@
 package week4.day17.employee_search;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class EmployeeSearch {
-  EmployeeSearchFrame frame;
-  SearchPanel searchPanel;
-  ResultPanel resultPanel;
 
   public static void main(String[] args) {
+    EmployeeStore.loadEmployeesFile();
     Frame frame = new EmployeeSearchFrame().getFrame();
     frame.add(new SearchPanel().getPanel());
     frame.add(new ResultPanel().getPanel());
