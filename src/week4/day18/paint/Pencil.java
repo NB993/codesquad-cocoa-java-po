27861a1271b91180dc8ae229495a9620ce02962c
jaehongChoe <qnalsfj@gmail.com>
2021-11-24 17:30:32 +0900

    Pencil	연필 도구 클래스
    아직 자유 선 긋는 방법을 몰라서 가볍게 선언만 해둠.
    타입, 사이즈가 필요할 것 같아서 두 멤버는 미리 선언해뒀음.
    나중에 자유선 긋기 성공하면 enum공부해서 펜슬타입 이넘으로
    바꿔보기.

--- src/week4/day18/paint/Pencil.java
+++ src/week4/day18/paint/Pencil.java
@@ -1,2 +1,14 @@
-package week4.day18.paint;public class Pencil {
+package week4.day18.paint;
+
+import java.awt.Component;
+
+public class Pencil extends Component {
+  private int size;
+  private String pencilType;
+
+  public Pencil() {
+    this.size = 10;
+    this.pencilType = "●";
+  }
 }
+
