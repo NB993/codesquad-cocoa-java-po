f3818076f74de56d9a36402f9405dc0c3dd3dbf8
jaehongChoe <qnalsfj@gmail.com>
2021-11-24 16:25:46 +0900

    MousePoint	그림을 그리기 위한 좌표 클래스
    start좌표와 end좌표를 담기 위해 사용.
    PaintFrame 클래스의 멤버.

--- src/week4/day18/paint/MousePoint.java
+++ src/week4/day18/paint/MousePoint.java
@@ -1,2 +1,10 @@
-package week4.day18.paint;public class MousePoint {
+package week4.day18.paint;
+
+import java.awt.Point;
+
+public class MousePoint extends Point {
+
+  public MousePoint() {
+    super();
+  }
 }
