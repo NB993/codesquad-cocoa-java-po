f2a3cf6e38fbb6034d164e2229115b896a767ff8
jaehongChoe <qnalsfj@gmail.com>
2021-11-24 18:00:42 +0900

    ToolBoxPanel	멤버변수 변경, makeButtons()추가
    ToolBox클래스를 삭제하고 ToolBoxPanel에서 Button객체들을 관리.
    makeButtons : Button객체들을 만들어서 리턴하는 메서드.

--- src/week4/day18/paint/ToolBoxPanel.java
+++ src/week4/day18/paint/ToolBoxPanel.java
@@ -1,14 +1,32 @@
 package week4.day18.paint;
 
+import java.awt.Button;
+import java.awt.GridLayout;
 import java.awt.Panel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToolBoxPanel extends Panel {
+  String[] buttonLabels = {"연필", "직선", "사각형", "원", "세모", "지우개"};
+  List<Button> toolsBtn;
+  Eraser eraser;
+  Pencil pencil;
 
   public ToolBoxPanel() {
-    initPanel();
+    this.toolsBtn = makeButtons();
+    setLayout(new GridLayout(1, 6));
+  }
 
+  private List<Button> makeButtons() {
+    List<Button> returnButtons = new ArrayList<>();
+    for (String label : buttonLabels) {
+      returnButtons.add(new Button(label));
+    }
+
+    return returnButtons;
   }
 
   private void initPanel() {
+
   }
 }
