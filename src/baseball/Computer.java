a33edf56fadd3ff571f64eb28905b480dee6406f
jaehongChoe <qnalsfj@gmail.com>
2021-11-27 10:27:10 +0900

    [baseball] Computer클래스 정답 생성 및 정답 리턴 기능 추가

--- src/baseball/Computer.java
+++ src/baseball/Computer.java
@@ -1,5 +1,39 @@
 package baseball;
 
-public class Computer {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Computer {
+  private final List<String> answer;
+  private final List<String> numbers;
+
+  public Computer() {
+    this.numbers = addNumbers();
+    this.answer = makeRandomAnswer();
+  }
+
+  private List<String> addNumbers() {
+    List<String> numbers = new ArrayList<>();
+    numbers.add("1");
+    numbers.add("2");
+    numbers.add("3");
+    numbers.add("4");
+    numbers.add("5");
+    numbers.add("6");
+    numbers.add("7");
+    numbers.add("8");
+    numbers.add("9");
+
+    return numbers;
+  }
+
+  private List<String> makeRandomAnswer() {
+    Collections.shuffle(numbers);
+    return (List<String>) numbers.stream().limit(3);
+  }
+
+  public List<String> getAnswer() {
+    return answer;
+  }
 }
