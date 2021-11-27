b9c08dd9e7a29dfb463ff8b562006e8fdaf8dffe
jaehongChoe <qnalsfj@gmail.com>
2021-11-27 10:10:21 +0900

    [baseball] 유저 정답 입력 기능 추가

--- src/baseball/GameBoard.java
+++ src/baseball/GameBoard.java
@@ -1,5 +1,21 @@
 package baseball;
 
 public class GameBoard {
+  private boolean isGameGoingOn;
+  private final User user;
+  private final Computer computer;
 
+  public GameBoard() {
+    this.isGameGoingOn = true;
+    this.user = new User();
+    this.computer = new Computer();
+  }
+
+  public boolean isGoingOn() {
+    return isGameGoingOn;
+  }
+
+  public void start() {
+
+  }
 }
--- src/baseball/User.java
+++ src/baseball/User.java
@@ -3,6 +3,17 @@ package baseball;
 import java.util.Scanner;
 
 public class User {
+  private final Scanner sc = new Scanner(System.in);
 
+  private String getAnswer() {
+    String answer = "";
+    try {
+      answer = sc.nextLine();
+      Integer.parseInt(answer); //숫자입력 확인 예외처리용도
+    } catch (NumberFormatException e) {
+      System.out.println("숫자만 입력해주세요.");
+    }
+    return answer;
+  }
 
 }
