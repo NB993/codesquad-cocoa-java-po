e55be92bfd494594f7a5102740fde83f31bcd9b8
jaehongChoe <qnalsfj@gmail.com>
2021-11-29 02:47:46 +0900

    [baseball] 유저 인풋 유효성 검사 코드 수정

--- src/baseball/User.java
+++ src/baseball/User.java
@@ -10,21 +10,36 @@ public class User {
   private final Scanner sc = new Scanner(System.in);
 
   public String inputNumber() throws IllegalArgumentException {
-    String answer = "";
+    String answer = sc.nextLine();
+    inputValidateCheck(answer);
+    return answer;
+  }
+
+  private void inputValidateCheck(String answer) throws IllegalArgumentException {
+    checkIfInputHasNumberOnly(answer);
+    checkIfInputLengthIsThree(answer);
+    checkIfInputHasDulplicateNumber(answer);
+  }
+
+  private void checkIfInputHasDulplicateNumber(String answer) {
+    Set<String> answerSet = new HashSet<>(List.of(answer.split("")));
+    if (answer.length() == 3 && answerSet.size() < 3) {
+      throw new DuplicateNumberException("중복되지 않은 숫자를 입력해 주세요.");
+    }
+  }
+
+  private void checkIfInputLengthIsThree(String answer) throws UnderThreeInputString {
+    if (!answer.equals("0") && answer.length() < 3) {
+      throw new UnderThreeInputString("세 자리 숫자를 입력해주세요.");
+    }
+  }
+
+  private void checkIfInputHasNumberOnly(String answer) throws NumberFormatException {
     try {
-      answer = sc.nextLine();
       Integer.parseInt(answer);
-      Set<String> answerSet = new HashSet<>(List.of(answer.split("")));
-      if (!answer.equals("0") && answer.length() < 3) {
-        throw new UnderThreeInputString("세 자리 숫자를 입력해주세요.");
-      }
-      if (answer.length() == 3 && answerSet.size() < 3) {
-        throw new DuplicateNumberException("중복되지 않은 숫자를 입력해 주세요.");
-      }
     } catch (NumberFormatException e) {
       throw new NumberFormatException("숫자만 입력해주세요.");
     }
-    return answer;
   }
 
 }
