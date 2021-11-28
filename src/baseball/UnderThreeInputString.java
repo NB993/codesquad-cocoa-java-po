807e7f589f8af48fa0e5464f18d0f813a95b7e97
jaehongChoe <qnalsfj@gmail.com>
2021-11-29 02:25:31 +0900

    [baseball] User인풋 커스텀 예외 추가

---
+++ src/baseball/UnderThreeInputString.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class UnderThreeInputString extends IllegalArgumentException{
+
+  public UnderThreeInputString(String msg) {
+    super(msg);
+  }
+
+}
--- src/baseball/User.java
+++ src/baseball/User.java
@@ -1,14 +1,29 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class User {
 
   private final Scanner sc = new Scanner(System.in);
 
-  public String inputNumber() throws NumberFormatException {
-    String answer = sc.nextLine();
-    Integer.parseInt(answer);
+  public String inputNumber() throws IllegalArgumentException {
+    String answer = "";
+    try {
+      answer = sc.nextLine();
+      Integer.parseInt(answer);
+      Set<String> answerSet = new HashSet<>(List.of(answer.split("")));
+      if (answer.length() < 3) {
+        throw new UnderThreeInputString("세 자리 숫자를 입력해주세요.");
+      }
+      if (answer.length() == 3 && answerSet.size() < 3) {
+        throw new DuplicateNumberException("중복되지 않은 숫자를 입력해 주세요.");
+      }
+    } catch (NumberFormatException e) {
+      throw new NumberFormatException("숫자만 입력해주세요.");
+    }
     return answer;
   }
 
