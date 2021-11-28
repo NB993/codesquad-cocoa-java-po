0419643b9585ebc3060d3cf2efbbf1fbda594bc2
jaehongChoe <qnalsfj@gmail.com>
2021-11-28 17:44:01 +0900

    [baseball] User클래스 인풋 메서드 하나로 통합
    숫자를 입력했는지를 확인하고, 에러발생시 인풋을 받은 곳에서
    예외처리하도록 함

--- src/baseball/GameBoard.java
+++ src/baseball/GameBoard.java
@@ -16,40 +16,40 @@ public class GameBoard {
   }
 
   public void start() {
-    computer.makeRandomAnswer();
     computer.askQuestionOf(user);
-    printAnswerCount();
     checkIsAnswerCorrect();
   }
 
-  private void checkIsAnswerCorrect() {
-    if (computer.getStrike() == 3) {
-      askUserWantNextGame(user);
-    }
-  }
-
-  private void askUserWantNextGame(User user) {
+  private void askIfUserWantNextGame(User user) {
     System.out.print("다음 게임을 진행하시려면 아무 숫자나 눌러주세요.");
-    if (user.isReadyForNextGame()) {
-      return;
+
+    try {
+      user.inputNumber();
+      computer.reset();
+    } catch (NumberFormatException e) {
+      System.out.println("게임종료");
+      setIsGameGoingOn(false);
     }
-    setIsGameGoingOn(false);
   }
 
   private void setIsGameGoingOn(boolean b) {
     isGameGoingOn = b;
   }
 
-  private void printAnswerCount() {
+  private void checkIsAnswerCorrect() {
     if (computer.getStrike() == 3) {
       System.out.println(computer.getAnswer() + "... 정답입니다!");
+      askIfUserWantNextGame(user);
+      computer.resetCount();
       return;
     }
     if (computer.getStrike() == 0 && computer.getBall() == 0) {
       System.out.println("낫싱!");
+      computer.resetCount();
       return;
     }
     System.out.println(computer.getStrike() + "스트라이크 " + computer.getBall() + "볼");
+    computer.resetCount();
   }
 
 }
--- src/baseball/User.java
+++ src/baseball/User.java
@@ -3,27 +3,13 @@ package baseball;
 import java.util.Scanner;
 
 public class User {
+
   private final Scanner sc = new Scanner(System.in);
 
-  public String submitAnswer() {
-    String answer = "";
-    try {
-      answer = sc.nextLine();
-      Integer.parseInt(answer); //숫자입력 확인 예외처리용도
-    } catch (NumberFormatException e) {
-      System.out.println("숫자만 입력해주세요.");
-    }
+  public String inputNumber() throws NumberFormatException {
+    String answer = sc.nextLine();
+    Integer.parseInt(answer);
     return answer;
   }
 
-  public boolean isReadyForNextGame() {
-    String answer = "";
-    try {
-      answer = sc.nextLine();
-      Integer.parseInt(answer);
-    } catch (NumberFormatException e) {
-      return false;
-    }
-    return true;
-  }
 }
