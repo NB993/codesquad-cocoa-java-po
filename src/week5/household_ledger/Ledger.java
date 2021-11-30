22d7d92edfce8649c565ebf0dc9fcaa86a75383f
jaehongChoe <qnalsfj@gmail.com>
2021-11-30 16:32:54 +0900

    [household_ledger] 브랜치 삭제용 커밋
    기능별로 pr을 따기 위해 일단 브랜치를 정리.

--- src/week5/household_ledger/Ledger.java
+++ src/week5/household_ledger/Ledger.java
@@ -1,5 +1,18 @@
 package week5.household_ledger;
 
-public class Ledger {
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+public class Ledger extends DefaultTableModel {
+
+  public Ledger() {
+
+  }
+
+  private List<LedgerItem> loadLedgerItems() {
+    //TODO: 파일로 ledeger리스트 불러오기
+
+    return null;
+  }
 
 }
--- src/week5/household_ledger/Main.java
+++ src/week5/household_ledger/Main.java
@@ -1,5 +1,7 @@
 package week5.household_ledger;
 
+import java.time.LocalDate;
+
 public class Main {
 
   public static void main(String[] args) {
