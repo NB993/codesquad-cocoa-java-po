553ff395c0769fff7e72f7141c88162a3d380ed0
jaehongChoe <qnalsfj@gmail.com>
2021-11-30 23:23:05 +0900

    [household_ledger] TablePanel 메서드 분리

---
+++ src/week5/household_ledger/LedgerModel.java
@@ -0,0 +1,11 @@
+package week5.household_ledger;
+
+import javax.swing.table.DefaultTableModel;
+
+public class LedgerModel extends DefaultTableModel {
+
+  public LedgerModel(String[][] modelData, String[] columns) {
+    super(modelData, columns);
+  }
+
+}
